3f614000fa95d6d1ff173a443b15fe90abc7f17d
Bananaperson21 <75265949+Bananaperson21@users.noreply.github.com>
2020-12-04 12:12:25 -0500

    Update game1.js

--- game1.js
+++ game1.js
@@ -12,7 +12,7 @@ function startgame () {
 if (game = true) {
   while (0 === 0) {
     
-    
+    document.getElementById("game").style.backgroundImage = "url("https://e7.pngegg.com/pngimages/546/604/png-clipart-water-free-content-water-background-s-blue-angle.png")";
     
   }
 }

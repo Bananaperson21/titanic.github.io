d0db2ad64e238957544e804cf3e93d57219294f2
Bananaperson21 <75265949+Bananaperson21@users.noreply.github.com>
2020-12-04 12:00:09 -0500

    Update game1.js

--- game1.js
+++ game1.js
@@ -2,3 +2,17 @@
 var game = null;
 var titanicX = event.clientX;
 
+//game stuff
+document.getElementById('button').addEventListener("click",startgame);
+
+function startgame () {
+  game = true;
+};
+
+if (game = true) {
+  while (0 === 0) {
+    
+    
+    
+  }
+}
